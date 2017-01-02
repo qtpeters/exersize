6a4541360a7f3dbb94b8b511feef24c7b1f296c3
qtpeters <quinn.t.peters@gmail.com>
2017-01-02 11:56:11 -0500

    Managed to get json coming to the UI

--- server/server.js
+++ server/server.js
@@ -1,14 +1,31 @@
 #!/usr/bin/env nodejs
 "use strict";
 
+const database = "workout";
+
 const express = require( 'express' );
 const app = express();
+const mongo = require( "mongodb" ).MongoClient
+const url = `mongodb://localhost:27017/${database}`
 
 let port = 6699;
 
 app.use( express.static( 'client' ) );
+app.get( "/exersizes", ( req, res ) => {
+	mongo.connect( url, ( err, db ) => {
+		
+		let exersizes = db.collection( 'exersizes' ).find();
+		let promise = exersizes.limit( 1 ).toArray();
+		promise.then( ( doc ) => {
+			let json = JSON.stringify( doc[0] );
+			res.send( `<h2>${json}</h2>` );
+		});
+
+		db.close();
+	});
+});
 
 app.listen( port, () => {
-  console.log( `Example app listening on port ${port}!` )
+  console.log( `Workout app listening on port ${port}!` )
 });
 
