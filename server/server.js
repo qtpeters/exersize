a5e1cfa96b057995ff89b7014178ac4f4c2d608f
qtpeters <quinn.t.peters@gmail.com>
2017-07-18 21:30:12 -0400

    Changed some workout data

--- server/server.js
+++ server/server.js
@@ -1,19 +1,41 @@
-#!/usr/bin/env nodejs
+#!/usr/bin/env node
 "use strict";
 
 const database = "workout";
 
 const express = require( 'express' );
 const app = express();
+
 const mongo = require( "mongodb" ).MongoClient
 const url = `mongodb://localhost:27017/${database}`
+
+const fs = require( 'fs' );
 
 let port = 6699;
 
 app.use( express.static( 'client' ) );
-app.get( "/exersizes", ( req, res ) => {
+app.get( "/exersizes/:how", ( req, res ) => {
+
+	switch ( req.params.how === 'db' ) {
+		case 'db':
+			_getFromDB( res );
+			break;
+		default: 
+			_getFromFile( res );
+	}
+});
+
+app.listen( port, () => {
+  console.log( `Workout app listening on port ${port}!` )
+});
+
+function _getFromFile( res ) {
+	res.send( fs.readFileSync( '../json/exersizes.json' ) );
+}
+
+function _getFromDB( res ) {
 	mongo.connect( url, ( err, db ) => {
-		
+				
 		let exersizes = db.collection( 'exersizes' ).find();
 		let promise = exersizes.limit( 1 ).toArray();
 		promise.then( ( doc ) => {
@@ -23,9 +45,4 @@ app.get( "/exersizes", ( req, res ) => {
 
 		db.close();
 	});
-});
-
-app.listen( port, () => {
-  console.log( `Workout app listening on port ${port}!` )
-});
-
+}
